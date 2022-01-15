8bce109aca826b044220489dcdb071d63c024bb0
nhungnth90 <charlie.nguyen.102@gmail.com>
2022-01-15 18:40:25 +0700

    update 15/01

--- source/desktop/js/custom.js
+++ source/desktop/js/custom.js
@@ -12,3 +12,31 @@ $('.search-icon').on('click', function() {
 $('#menu_dropdown').on('click', function() {
     $('.dropdown').toggleClass("active");
 });
+
+
+/*--- Tabs ---*/
+// Multimedia
+var tabLinks = document.querySelectorAll(".tablinks");
+var tabContent = document.querySelectorAll(".tabcontent");
+
+tabLinks.forEach(function(el) {
+    el.addEventListener("click", openTabs);
+});
+
+
+function openTabs(el) {
+    var btn = el.currentTarget;
+    var electronic = btn.dataset.electronic;
+
+    tabContent.forEach(function(el) {
+        el.classList.remove("active");
+    });
+
+    tabLinks.forEach(function(el) {
+        el.classList.remove("active");
+    });
+
+    document.querySelector("#" + electronic).classList.add("active");
+
+    btn.classList.add("active");
+}
